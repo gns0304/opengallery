0e9c6e147fd02a307674d62a518a4a1df7aae8fa
gns0304 <gns0304@naver.com>
2025-08-12 20:19:35 +0900

    chore: configure Django media settings for image uploads
    - MEDIA_URL과 MEDIA_ROOT를 설정하여 이미지 업로드 및 접근 경로를 구성
    - 개발 환경에서 media 파일 서빙을 위해 urls.py에 static() 설정을 추가

--- opengallery/settings.py
+++ opengallery/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 
